89e997fd4eaa91b741e718a672d393390ee7c439
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-25 13:32:11 +0700

    Fixing splash screen

--- app/src/main/java/com/github/syafiqq/hukumpro/presentation/activity/welcome/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/github/syafiqq/hukumpro/presentation/activity/welcome/splashscreen/SplashScreenActivity.kt
@@ -1,15 +1,36 @@
 package com.github.syafiqq.hukumpro.presentation.activity.welcome.splashscreen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.github.syafiqq.hukumpro.R
 import com.github.syafiqq.hukumpro.presentation.fragment.welcome.splashscreen.SplashScreenFragment
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class SplashScreenActivity : AppCompatActivity() {
-    val viewModel: SplashScreenActivityViewModel by viewModels()
+    private lateinit var vwContent: View
+
+    private val viewModel: SplashScreenActivityViewModel by viewModels()
+
+    private val hideHandler = Handler(Looper.getMainLooper())
+
+    @SuppressLint("InlinedApi")
+    private val hidePart2Runnable = Runnable {
+        vwContent.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +40,55 @@ class SplashScreenActivity : AppCompatActivity() {
                 .replace(R.id.root, SplashScreenFragment.newInstance())
                 .commitNow()
         }
+
+        initViews()
+        initData()
+        initEvents()
+        initViewModels()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        Timber.v("CurrentLog - onPostCreate")
+        super.onPostCreate(savedInstanceState)
+        hide()
+    }
+
+    override fun onStart() {
+        Timber.v("CurrentLog - onStart")
+        super.onStart()
+        lifecycleScope.launchWhenStarted {
+        }
+    }
+
+    override fun onStop() {
+        Timber.v("CurrentLog - onStop")
+        super.onStop()
+    }
+
+    private fun initViews() {
+        vwContent = findViewById(R.id.root)
+    }
+
+    private fun initData() {
+    }
+
+    private fun initEvents() {
+    }
+
+    private fun initViewModels() {
+    }
+
+    fun gotoMainMenu() {
+        finish()
+    }
+
+    private fun hide() {
+        supportActionBar?.hide()
+
+        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    companion object {
+        private const val UI_ANIMATION_DELAY = 300
     }
 }
