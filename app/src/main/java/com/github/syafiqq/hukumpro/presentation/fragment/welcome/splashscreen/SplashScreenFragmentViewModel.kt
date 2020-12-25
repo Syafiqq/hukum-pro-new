770c9c27b32c56ff6a68725698c4313efa7de5b7
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-25 15:50:11 +0700

    add splash screen fragment view model

--- app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
+++ app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
@@ -2,12 +2,26 @@ package com.github.syafiqq.hukumpro.presentation.fragment.welcome.splashscreen
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.github.syafiqq.domain.entity.repoversion.RepositoryVersionUpdateAvailable
+import com.github.syafiqq.domain.entity.repoversion.RepositoryVersionUpdateRequired
 import com.github.syafiqq.domain.usecase.repoversion.CheckLocalVersionUseCase
 
 class SplashScreenFragmentViewModel @ViewModelInject constructor(
     var checkLocalVersionUseCase: CheckLocalVersionUseCase
 ) : ViewModel() {
     suspend fun checkVersion() {
+        try {
+            val versionState = checkLocalVersionUseCase.execute()
+            when (versionState) {
+                is RepositoryVersionUpdateRequired -> {
+                }
+                is RepositoryVersionUpdateAvailable -> {
+                }
+                else -> {
+                }
+            }
+        } catch (e: Throwable) {
 
+        }
     }
 }
