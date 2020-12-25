b63dd68036b092022629fd1728764ece472e7d87
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-25 16:41:03 +0700

    add some use case

--- app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
+++ app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.github.syafiqq.domain.entity.repoversion.RepositoryVersionUpdateRequired
 import com.github.syafiqq.domain.usecase.repoversion.CheckLocalVersionUseCase
+import com.github.syafiqq.domain.usecase.repoversion.UpdateRepositoryVersionUseCase
 
 class SplashScreenFragmentViewModel @ViewModelInject constructor(
-    var checkLocalVersionUseCase: CheckLocalVersionUseCase
+    var checkLocalVersionUseCase: CheckLocalVersionUseCase,
+    var updateRepositoryVersionUseCase: UpdateRepositoryVersionUseCase
 ) : ViewModel() {
     val _error: MutableLiveData<Exception> = MutableLiveData()
     val error: LiveData<Exception>
@@ -16,7 +18,24 @@ class SplashScreenFragmentViewModel @ViewModelInject constructor(
             return _error
         }
 
+    private fun actionNoUpdateNeeded() {
+
+    }
+
     suspend fun checkVersion() {
+        try {
+            val localVersion = checkLocalVersion()
+            if (localVersion == null) {
+                actionNoUpdateNeeded()
+                return
+            }
+
+        } catch (e: Exception) {
+
+        }
+    }
+
+    private suspend fun checkLocalVersion(): RepositoryVersionUpdateRequired? {
         try {
             when (checkLocalVersionUseCase.execute()) {
                 is RepositoryVersionUpdateRequired -> {
