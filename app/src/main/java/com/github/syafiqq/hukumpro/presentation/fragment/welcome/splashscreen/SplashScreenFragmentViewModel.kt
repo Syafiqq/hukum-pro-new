f39e7babfb61bce9c0dfb406a0cd120e136ff0f6
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-25 16:03:01 +0700

    add live data

--- app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
+++ app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
@@ -1,6 +1,8 @@
 package com.github.syafiqq.hukumpro.presentation.fragment.welcome.splashscreen
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.github.syafiqq.domain.entity.repoversion.RepositoryVersionUpdateRequired
 import com.github.syafiqq.domain.usecase.repoversion.CheckLocalVersionUseCase
@@ -8,6 +10,12 @@ import com.github.syafiqq.domain.usecase.repoversion.CheckLocalVersionUseCase
 class SplashScreenFragmentViewModel @ViewModelInject constructor(
     var checkLocalVersionUseCase: CheckLocalVersionUseCase
 ) : ViewModel() {
+    val _error: MutableLiveData<Throwable> = MutableLiveData()
+    val error: LiveData<Throwable>
+        get() {
+            return _error
+        }
+
     suspend fun checkVersion() {
         try {
             when (checkLocalVersionUseCase.execute()) {
