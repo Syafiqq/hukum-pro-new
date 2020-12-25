1070a7772fa4679230a098869e184e8164589876
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-25 14:28:45 +0700

    add check local version use case

--- app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
+++ app/src/main/java/com/github/syafiqq/hukumpro/presentation/fragment/welcome/splashscreen/SplashScreenFragmentViewModel.kt
@@ -2,5 +2,8 @@ package com.github.syafiqq.hukumpro.presentation.fragment.welcome.splashscreen
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.github.syafiqq.domain.usecase.repoversion.CheckLocalVersionUseCase
 
-class SplashScreenFragmentViewModel @ViewModelInject constructor() : ViewModel()
+class SplashScreenFragmentViewModel @ViewModelInject constructor(
+    var checkLocalVersionUseCase: CheckLocalVersionUseCase
+) : ViewModel()
