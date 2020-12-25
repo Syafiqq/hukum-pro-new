64c87a03507d8dfe06dd028b6dc85c135a63769e
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-25 16:13:15 +0700

    add defined throwable

---
+++ common-java/src/main/java/com/github/syafiqq/common/error/DefinedThrowable.kt
@@ -0,0 +1,12 @@
+package com.github.syafiqq.common.error
+
+open class DefinedThrowable(
+    override val message: String?,
+    override val cause: Throwable?,
+) : Throwable(message, cause) {
+    constructor(message: String?) : this(message, null)
+
+    constructor(cause: kotlin.Throwable?) : this(cause?.toString(), cause)
+
+    constructor() : this(null, null)
+}
