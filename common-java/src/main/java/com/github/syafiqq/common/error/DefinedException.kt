e5709e926a352599b6b1100f0e12c928d927af7f
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-26 08:30:39 +0700

    change exception

--- common-java/src/main/java/com/github/syafiqq/common/error/DefinedException.kt
+++ common-java/src/main/java/com/github/syafiqq/common/error/DefinedException.kt
@@ -3,7 +3,7 @@ package com.github.syafiqq.common.error
 open class DefinedException(
     override val message: String?,
     override val cause: Throwable?,
-) : Exception(message, cause) {
+) : RuntimeException(message, cause) {
     constructor(message: String?) : this(message, null)
 
     constructor(cause: kotlin.Throwable?) : this(cause?.toString(), cause)
--- datasource-cache-sharedpref/src/main/java/com/github/syafiqq/data/datasource/cache/sharedpref/util/error/SharedPreferenceException.kt
+++ datasource-cache-sharedpref/src/main/java/com/github/syafiqq/data/datasource/cache/sharedpref/util/error/SharedPreferenceException.kt
@@ -1,6 +1,4 @@
 package com.github.syafiqq.data.datasource.cache.sharedpref.util.error
 
-import com.github.syafiqq.common.error.DefinedException
-
 class NoSharedPreferenceInstanceException(cause: Throwable? = null) :
-    DefinedException("No Shared Preference Instance Found", cause)
+    RuntimeException("No Shared Preference Instance Found", cause)
