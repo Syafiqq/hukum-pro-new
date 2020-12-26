e9076e8557795333044ae0cbd528111badb26254
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-26 17:36:20 +0700

    add view model erro rhandler

---
+++ app/src/main/java/com/github/syafiqq/hukumpro/common/util/ViewModelErrorHandler.kt
@@ -0,0 +1,23 @@
+package com.github.syafiqq.hukumpro.common.util
+
+import com.github.syafiqq.common.error.ApiException
+import com.github.syafiqq.common.error.DefinedException
+import com.github.syafiqq.hukumpro.common.error.Cancel
+import com.github.syafiqq.hukumpro.common.error.Retry
+import com.github.syafiqq.hukumpro.common.error.ViewCompletableError
+import com.github.syafiqq.hukumpro.common.error.ViewRetryableError
+
+object ViewModelErrorHandler {
+    fun handleDefaultError(e: Exception, onRetry: () -> Void, onComplete: () -> Void): Exception {
+        return when (e) {
+            is DefinedException -> ViewCompletableError(e.message, e, callback = onComplete)
+            is ApiException -> ViewRetryableError(e.message, e) {
+                when (it) {
+                    is Cancel -> onComplete()
+                    is Retry -> onRetry()
+                }
+            }
+            else -> e
+        }
+    }
+}
