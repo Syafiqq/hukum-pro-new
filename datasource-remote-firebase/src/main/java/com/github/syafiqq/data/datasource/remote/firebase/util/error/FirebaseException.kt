350527e695f0f4e9eb3679666eed897230bbe1c3
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-27 12:11:28 +0700

    change to api exception

--- datasource-remote-firebase/src/main/java/com/github/syafiqq/data/datasource/remote/firebase/util/error/FirebaseException.kt
+++ datasource-remote-firebase/src/main/java/com/github/syafiqq/data/datasource/remote/firebase/util/error/FirebaseException.kt
@@ -1,17 +1,17 @@
 package com.github.syafiqq.data.datasource.remote.firebase.util.error
 
-import com.github.syafiqq.common.error.DefinedException
+import com.github.syafiqq.common.error.ApiException
 import com.github.syafiqq.common.error.ErrorCode
 
 object NoDataException :
-    DefinedException(
+    ApiException(
         ErrorCode.ERROR_DATA_NO_DATA_AVAILABLE.name,
         ErrorCode.ERROR_DATA_NO_DATA_AVAILABLE.code,
         null
     )
 
 class ParseDataException(cause: Throwable? = null) :
-    DefinedException(
+    ApiException(
         ErrorCode.ERROR_DATA_PARSE_DATA_FAILURE.name,
         ErrorCode.ERROR_DATA_PARSE_DATA_FAILURE.code,
         cause
