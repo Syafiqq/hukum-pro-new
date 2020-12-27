c4596ee4cce1c1197ea150a967b7230d8678000e
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-27 10:42:28 +0700

    reduce all possibilities

--- common-java/src/main/java/com/github/syafiqq/common/error/DefinedException.kt
+++ common-java/src/main/java/com/github/syafiqq/common/error/DefinedException.kt
@@ -2,13 +2,6 @@ package com.github.syafiqq.common.error
 
 open class DefinedException(
     override val message: String,
+    val code: String,
     override val cause: Throwable?,
-) : RuntimeException(message, cause) {
-    constructor(message: String?) : this(message ?: ErrorConstant.defaultErrorMessage, null)
-
-    constructor(cause: kotlin.Throwable?) : this(
-        cause?.toString() ?: ErrorConstant.defaultErrorMessage, cause
-    )
-
-    constructor() : this(ErrorConstant.defaultErrorMessage, null)
-}
+) : RuntimeException(message, cause)
