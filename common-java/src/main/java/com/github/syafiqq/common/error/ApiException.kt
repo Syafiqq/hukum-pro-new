e61c5bed5d920d126bcb07bb1d70ccbc303eba72
msyafiqq <id.muhammad.syafiq@gmail.com>
2020-12-27 10:44:57 +0700

    fix api exception

--- common-java/src/main/java/com/github/syafiqq/common/error/ApiException.kt
+++ common-java/src/main/java/com/github/syafiqq/common/error/ApiException.kt
@@ -2,13 +2,6 @@ package com.github.syafiqq.common.error
 
 open class ApiException(
     override val message: String,
+    override val code: String,
     override val cause: Throwable?,
-) : DefinedException(message, cause) {
-    constructor(message: String?) : this(message ?: ErrorConstant.defaultErrorMessage, null)
-
-    constructor(cause: kotlin.Throwable?) : this(
-        cause?.toString() ?: ErrorConstant.defaultErrorMessage, cause
-    )
-
-    constructor() : this(null ?: ErrorConstant.defaultErrorMessage, null)
-}
+) : DefinedException(message, code, cause)
--- common-java/src/main/java/com/github/syafiqq/common/error/DefinedException.kt
+++ common-java/src/main/java/com/github/syafiqq/common/error/DefinedException.kt
@@ -2,6 +2,6 @@ package com.github.syafiqq.common.error
 
 open class DefinedException(
     override val message: String,
-    val code: String,
+    open val code: String,
     override val cause: Throwable?,
 ) : RuntimeException(message, cause)
